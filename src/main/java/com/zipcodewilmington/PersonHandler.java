e6eba5b47b4b071309d13f4f41970119928459f6
nikki <nikki@Gbrads-Mac.local>
2020-11-03 18:15:31 -0500

    lab compelted

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -12,15 +12,23 @@ public class PersonHandler {
 
     public String whileLoop() {
         String result = "";
+
         // create a `counter`
         // while `counter` is less than length of array
-            // begin loop
-
+        // begin loop
+        Integer counter = -1;
+        while (counter < personArray.length -1) {  // what does adding this -1 do...?
+            counter++;
+            Person stringRepresentation =  personArray[counter];
+            String apple = stringRepresentation.toString();
+            result = result + apple;
+        }
                 // use `counter` to identify the `current Person` in the array
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
-
             // end loop
+        //return apple + result;
+
         return result;
     }
 
@@ -28,6 +36,13 @@ public class PersonHandler {
 
     public String forLoop() {
         String result = "";
+
+        for (int i = 0; i < personArray.length; i++) {
+            Person stringRepresentation =  personArray[i];
+            String apple = stringRepresentation.toString();
+            result = result + apple;
+        }
+
         // identify initial value
         // identify terminal condition
         // identify increment
@@ -48,7 +63,10 @@ public class PersonHandler {
         String result = "";
         // identify array's type
         // identify array's variable-name
-
+            for ( Person pick : personArray) {
+               String stringRepresentation = pick.toString();
+               result = result + stringRepresentation;
+            }
         // use the above discoveries to declare for-each-loop signature
             // begin loop
                 // get `string Representation` of `currentPerson`
